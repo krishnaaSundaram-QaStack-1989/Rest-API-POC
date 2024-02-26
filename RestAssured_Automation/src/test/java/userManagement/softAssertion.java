647a138e30d50482b61bff73d03a59ce83aaf982
krishnaaSundaram-QaStack-1989 <85032384+krishnaaSundaram-QaStack-1989@users.noreply.github.com>
2024-02-26 11:46:48 +0530

    Update softAssertion.java

--- RestAssured_Automation/src/test/java/userManagement/softAssertion.java
+++ RestAssured_Automation/src/test/java/userManagement/softAssertion.java
@@ -3,14 +3,19 @@ import static org.hamcrest.Matchers.*;
 import org.testng.annotations.Test;
 import org.testng.asserts.SoftAssert;
 
+import core.statusCode;
 import io.restassured.RestAssured;
 import io.restassured.response.Response;
+import utils.softAssertionUtils;
+
 import static org.hamcrest.MatcherAssert.assertThat;
 import static io.restassured.RestAssured.given;
 import java.util.Arrays;
 import java.util.List;
 
 public class softAssertion {
+
+	softAssertionUtils sa = new softAssertionUtils();	
 /*	
  * few examplesof hard assertion given below
  * assertEquals(actualcode, 200);
@@ -30,7 +35,8 @@ public class softAssertion {
 	// multiple assertions are to be done then we use soft assertion
 	// else hard assert we can use 
 	// To Do - Create and move this to utility class
-	@Test
+	
+	@Test (description = "Without utility class we're doing this")
 	public void softAssert() {
 		SoftAssert sa = new SoftAssert();
 		System.out.println("soft assert");
@@ -38,4 +44,62 @@ public class softAssertion {
 		sa.assertTrue(true);
 		sa.assertAll();
 	}
+	
+	@Test (description = "1 - using soft assertion utility class")
+	public void softAssertions() {
+		softAssertionUtils sa = new softAssertionUtils();
+		System.out.println("soft assert with utility class");
+		sa.assertTrue(true, "Test Message");
+		sa.assertAll();
+	}
+	
+	@Test (description = "2 - using soft assertion utility class")
+	public void softAssertionsValidation() {
+		System.out.println("soft assert with utility class");
+		// Set base URI for the API
+		RestAssured.baseURI = "https://reqres.in/api";
+		
+		Response response = given()
+		.queryParam("page", 2)
+		.when()
+		.get("/users")
+		.then()
+		.statusCode(200)
+		.extract()
+		.response();
+		
+		response.then().body("data", hasSize(6));
+		
+		sa.assertEquals(response.getStatusCode(), statusCode.SUCCESS.code, "status code is not 200");
+		sa.assertAll();
+		System.out.println("softAssertionsValidation passed");
+		
+	}
+	
+	@Test (description = "3- using soft assertion utility class force failing for assertion check")
+	public void softAssertionsValidationForceFailing() {
+		System.out.println("soft assert with utility class");
+		// Set base URI for the API
+		RestAssured.baseURI = "https://reqres.in/api";
+		
+		Response response = given()
+		.queryParam("page", 2)
+		.when()
+		.get("/users")
+		.then()
+		.statusCode(200)
+		.extract()
+		.response();
+		
+		response.then().body("data", hasSize(6));
+		
+		sa.assertEquals(response.getStatusCode(), statusCode.NO_CONTENT.code, "status code is not 200");
+		sa.assertAll(); // mandatory else test results will show as passed
+		System.out.println("softAssertionsValidationForceFailing passed");
+		
+	}
+	
+	
+	
 }
+
