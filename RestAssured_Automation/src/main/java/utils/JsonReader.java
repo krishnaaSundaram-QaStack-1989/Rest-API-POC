4a333d0c03abaca6eba6a2409c1f3b0e844befd6
krishnaaSundaram-QaStack-1989 <85032384+krishnaaSundaram-QaStack-1989@users.noreply.github.com>
2024-03-18 15:41:21 +0530

    Update JsonReader.java

--- RestAssured_Automation/src/main/java/utils/JsonReader.java
+++ RestAssured_Automation/src/main/java/utils/JsonReader.java
@@ -2,39 +2,6 @@ package utils;
 import java.io.File;
 import java.io.IOException;
 import org.apache.commons.io.FileUtils;
-import org.json.simple.JSONObject;
-import org.json.simple.parser.JSONParser;
-import bsh.ParseException;
-
-public class jsonReader {
-
-	public static String getTestData(String key) throws IOException, ParseException, org.json.simple.parser.ParseException {
-		@SuppressWarnings("unused")
-		String value;
-		return value = (String) getjsondata().get(key); // input is the key
-	}
-	
-	public static JSONObject getjsondata() throws IOException, org.json.simple.parser.ParseException {
-		
-		File filename = new File("src\\main\\resources\\TestData\\testData.json");
-		
-		//convert json file to string 
-		
-		String json = FileUtils.readFileToString(filename, "UTF-8");
-		//parse string intoobject
-		
-		Object obj = new JSONParser().parse(json);
-		
-		//give json object  so that i can return it to function everytie it get called
-		
-		JSONObject jsonObject = (JSONObject) obj;
-		return jsonObject;
-	}
-
-	package utils;
-import java.io.File;
-import java.io.IOException;
-import org.apache.commons.io.FileUtils;
 import org.json.simple.JSONArray;
 import org.json.simple.JSONObject;
 import org.json.simple.parser.JSONParser;
@@ -65,20 +32,20 @@ public class jsonReader {
 		return jsonObject;
 	}	
 	
-	public static JSONArray getJsonArray(String key) throws IOException, ParseException, org.json.simple.parser.ParseException {
+	public static JSONArray getJsonArray(String key) 
+			throws IOException, ParseException, org.json.simple.parser.ParseException {
 		JSONObject jsonObject = getjsondata();
 		JSONArray JsonArray = (JSONArray) jsonObject.get(key);
 		return JsonArray;
 	}
-	
-	public static Object getJsonArrayData(int index) throws ParseException, IOException, 
-	org.json.simple.parser.ParseException {
+	// eg string can be languages and index can be values inside the languages json array
+	// we can also have another array objest which we can call in key and value in index
+	// which can be called
+	//object is used as data can change from string , nos ets. To handle any form
+	// of data  we're using this Object
+	public static Object getJsonArrayData(String key, int index) 
+			throws ParseException, IOException, org.json.simple.parser.ParseException {
 		JSONArray language = getJsonArray("languages");
 		return language.get(index);
 	}
-	
-	
-}	
-
-
-}	
+}
